09f8040a24ff1490543a97466f9cf8b52c22ea62
shizakira <ikirachn@gmail.com>
2026-02-17 00:54:50 +0500

    add AddItemInput validation

--- internal/dto/add_item.go
+++ internal/dto/add_item.go
@@ -1,8 +1,19 @@
 package dto
 
+import (
+	val "github.com/go-ozzo/ozzo-validation"
+)
+
 type AddItemInput struct {
-	UserID int  `json:"user_id"`
-	SkuID  int  `json:"sku_id"`
-	Count  uint `json:"count"`
+	UserID int
+	SkuID  int
 	Count  int
+}
+
+func (d AddItemInput) Validate() error {
+	return val.ValidateStruct(&d,
+		val.Field(&d.UserID, val.Required, val.Min(1).Error("sku_id must be >= 1")),
+		val.Field(&d.SkuID, val.Required, val.Min(1).Error("sku_id must be >= 1")),
+		val.Field(&d.Count, val.Required, val.Min(1).Error("count must be >= 1")),
+	)
 }
